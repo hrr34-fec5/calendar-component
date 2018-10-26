9538b610d3551cbbe24eb5c4d055e175aa872450
Stephen Weiss <stephencweiss@gmail.com>
2018-10-26 11:18:48 -0700

    Tweaks to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
-  "extends": "eslint-config-airbnb"
+  "extends": "eslint-config-airbnb",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  }
 }
 
