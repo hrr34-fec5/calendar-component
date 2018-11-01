a7f5c3858452683ac6172a9cf5282e3df18012fa
Stephen Weiss <stephencweiss@gmail.com>
2018-11-01 11:21:16 -0700

    Added nights and minimimum nights to the app's state
    These data elements will allow us to know what's available and update the calendar component via props.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,12 +5,18 @@ import Calendar from './components/calendar.jsx'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      availableNights: [],
+      minimumNights: undefined
+    };
   }
   render() {
     return (
       <div>
         <h1>Hello World</h1>
+        <p> There are {this.state.availableNights.length} available nights </p>
+        <p> The minimum nights for this listing is: {this.state.minimumNights ? this.state.minimumNights : 0 } </p>
+        <Calendar />
       </div>);
   }
 }
