eb9bd88ee0d7d08fd46a9df12912fdc14a2ae167
Stephen Weiss <stephencweiss@gmail.com>
2018-11-01 19:51:42 -0700

    Added basic testing functionality
    Tested a mocked up React component and a single endpoint for /availableNights using Jest.
    
    Also modified .circleCI to now include tests?

---
+++ server/server.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+// const {expect} = require('chai');
+
+const { app } = require('./server.js');
+
+
+// describe('/guest endpoints')
+// describe('/host endpoints')
+// describe('/listing endpoints')
+// describe('/booking endpoints')
+describe('/availableNights endpoints', () => {
+  it('should return', () => {
+    request(app)
+    .get('/nights')
+    .expect(200)
+    .end();
+  })
+})
