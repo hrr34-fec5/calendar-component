bcd8fe28dc9a7888286ff37b754e2078fd7a4ebb
Stephen Weiss <stephencweiss@gmail.com>
2018-11-02 20:13:30 -0700

    Nights are selectable; start/end dates get set
    I can now select a valid night as a start date and select a future night as an end date.
    
    If a night is selected that is *before* the start date, it is set as the start date.
    
    Things to fix:
    1. There's no way to clear start/end dates
    2. End nights with unavailable nights between start and end are accepted for picking

--- client/src/components/calendar.jsx
+++ client/src/components/calendar.jsx
@@ -9,93 +9,66 @@ import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'rea
 class Calendar extends React.Component {
   constructor(props) {
     super(props);
-    let focusedInput = null;
-    if (props.autoFocus) {
-      focusedInput = START_DATE;
-    } else if (props.autoFocusEndDate) {
-      focusedInput = END_DATE;
-    }
-
+    
     this.state = {
-      focusedInput,
+      focusedInput: props.autoFocusEndDate ? 'endDate' : 'startDate',
       startDate: null,
       endDate: null,
       availableNights: this.props.availableNights,
-      minimumNights: this.props.minimumNights
+      minimumNights: this.props.minimumNights,
+      showInputs: true,
     };
 
     this.onDatesChange = this.onDatesChange.bind(this);
     this.onFocusChange = this.onFocusChange.bind(this);
     this.isDayBlocked = this.isDayBlocked.bind(this);
-
   }
-
+  
   onDatesChange({ startDate, endDate }) {
-    console.log(`The Date changed, startDate -------> `, startDate)
-    console.log(`The Date changed, endDate -------> `, endDate)
-    // const { stateDateWrapper } = this.props; // Believe this is for internationalization
     this.setState({
       startDate: startDate,
       endDate: endDate,
-      minimumNights: this.props.minimumNights
     })
   }
 
+
   onFocusChange(focusedInput) {
     console.log(`The focused input is ----->`, focusedInput)
-    this.setState({ focusedInput })
+    this.setState({
+      focusedInput: !focusedInput ? "startDate" : focusedInput
+    })
   }
 
   isDayBlocked(selectedDate) {
-    console.log(`The selected day from isDayBlocked -->`, selectedDate)
     let numberOfAvailableNights = this.props.availableNights.length;
-    console.log(`The numberOfAvailableNights is -->`, numberOfAvailableNights)
     for ( let night = 0; night < numberOfAvailableNights; night++) {
       let currentNight = moment(this.props.availableNights[night].startDate);
-      console.log(`The current night is --> `, currentNight)
       if (selectedDate.isSame(moment(currentNight), "day")) {
-        console.log(`the selectedDate --> `, selectedDate);
-        console.log(`Is the same as --> `, currentNight);
-        return true;
-      }
-      else if (currentNight.isAfter(selectedDate, "day")) {
-        console.log(`the currentNight --> `, currentNight);
-        console.log(`Is after the selectedDate --> `, selectedDate);
         return false;
       }
+      else if (currentNight.isAfter(selectedDate, "day")) {
+        return true;
+      }
     } 
-    console.log(`the selectedDate -->`, selectedDate)
-    console.log(`is NOT available`)
-    return false;
-
+    return true;
   }
-  // isDayBlocked: PropTypes.func,
 
   render() {
-    console.log(`the props passed to calendar ----> `, this.props)
     return (
-      <div>
+      <div>        
         <h1> The calendar component </h1>
-        {/* <DateRangePicker 
+        <DayPickerRangeController
           startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-          startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
           endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-          endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-          onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
           focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-          onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-        /> */}
-        <div>
-          <DayPickerRangeController
-            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-            onDatesChange={this.onDatesChange} // PropTypes.func.isRequired,
-            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-            onFocusChange={this.onFocusChange} // PropTypes.func.isRequired,
-            minimumNights={this.state.minimumNights} // PropTypes.number
-            isDayBlocked={this.isDayBlocked}
-          />
-        </div>
+          minimumNights={this.state.minimumNights} // PropTypes.number
+          onDatesChange={this.onDatesChange} // PropTypes.func.isRequired,
+          onFocusChange={this.onFocusChange} // PropTypes.func.isRequired,
+          isDayBlocked={this.isDayBlocked}
+          keepOpenOnDateSelect={true}
+          noBorder={true}
+          numberOfMonths={2}
+        />
       </div>
     )
   }
