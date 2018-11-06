ecd28ca3d4e7cb7e92d6d6547cba362250e79e13
Stephen Weiss <stephencweiss@gmail.com>
2018-11-06 11:21:34 -0800

    Main app render test
    Added a test to confirm that the main app renders as expected

--- jestTests/index.test.js
+++ jestTests/index.test.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { App } from '../src/components/app';
+import App from '../client/src/components/app.jsx';
 import { wrap } from 'module';
 
 
-describe(' Main app renders ', () => {
-  test( 'render Hello World', () => {
-    const wrapper = mount(<App />);
-    // expect(wrapper.state().availableNights.length).toEqual(0);
-    // expect(wrapper.state().minimumNights).toEqual(1);
+describe('Main app renders', () => {
+  test( 'Render Hello World', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state().availableNights.length).toEqual(0);
+    expect(wrapper.state().minimumNights).toEqual(1);
     expect(wrapper.find('h1').text()).toEqual('Hello World');
   })
+  // test( 'What happens with test data' ) { 
+    // Mock out the api call 
+  // }
 })
