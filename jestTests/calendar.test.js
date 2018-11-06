9b9b6baf9d7c8d45556e464d3de2f33907fd6963
Stephen Weiss <stephencweiss@gmail.com>
2018-11-06 15:01:45 -0800

    Added a basic Calendar Component test
    Ensures that the calendar componenet renders with the appropriate header.

--- jestTests/calendar.test.js
+++ jestTests/calendar.test.js
@@ -1,5 +1,15 @@
-// expect button to mount
-// expect button to clear state if clicked
-  // setState
-  // click the button
-  // expect startDate === null
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Calendar from '../client/src/components/calendar';
+import { wrap } from 'module';
+
+
+describe('Calendar app renders', () => {
+  test( 'Render The Calendar Component', () => {
+    const wrapper = shallow(<Calendar />);
+    expect(wrapper.find('h1').text()).toEqual(' The calendar component ');
+  })
+  // test( 'What happens with test data' ) { 
+    // Mock out the api call 
+  // }
+})
